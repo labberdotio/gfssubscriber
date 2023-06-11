cf6a0f5784aa435b4156c0464e8d829c136c6df9
whoopsjohnnie <jgrundback@gmail.com>
2023-06-11 13:24:25 -0700

    Build docker images with branch name intact.
    This push script should also tag images with branch name so we know what
    it was built and pushed from.

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ set -ex
 # LOCAL_REGISTRY="registry.dev.appgoto.com"
 LOCAL_REGISTRY="${LOCAL_REGISTRY:-registry.dev.appgoto.com}"
 
+GIT_BRNCH=$(git rev-parse --abbrev-ref HEAD)
 GIT_SHA=$(git rev-parse HEAD | cut -c 1-8)
 
-IMAGE="${LOCAL_REGISTRY}/gfs-subscriber:latest-$GIT_SHA"
+IMAGE="${LOCAL_REGISTRY}/gfs-subscriber:${GIT_BRNCH}-${GIT_SHA}"
 LATEST_IMAGE="${LOCAL_REGISTRY}/gfs-subscriber:latest"
 
 docker build -t $IMAGE -f Dockerfile .
